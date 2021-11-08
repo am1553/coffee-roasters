67dee232f2478a503f6cbae36424333643b00c99
am1553 <aryanmehta2016@gmail.com>
2021-11-08 23:52:22 +0000

    fixed nav panel testing now

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const menuBtnImg = menuBtn.children[0];
 menuBtn.addEventListener("click", ()=> {
     if(menuBtnImg.getAttribute("src") === "assets/shared/mobile/icon-hamburger.svg") {
         menuBtnImg.setAttribute("src", "assets/shared/mobile/icon-close.svg");
-        document.querySelector(".navigation").style.top = "15vh";
+        document.querySelector(".navigation").style.top = "100px";
     } else {
         menuBtnImg.setAttribute("src", "assets/shared/mobile/icon-hamburger.svg");
         document.querySelector(".navigation").style.top = "-1000%";
