a57ee1fcae1af8aa3ef5f7935c39c163cfeae0e6
am1553 <aryanmehta2016@gmail.com>
2021-11-08 23:49:27 +0000

    navigation javascript added and now testing

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.getElementById("menu-button");
+
+const menuBtnImg = menuBtn.children[0];
+
+
+menuBtn.addEventListener("click", ()=> {
+    if(menuBtnImg.getAttribute("src") === "assets/shared/mobile/icon-hamburger.svg") {
+        menuBtnImg.setAttribute("src", "assets/shared/mobile/icon-close.svg");
+        document.querySelector(".navigation").style.top = "15vh";
+    } else {
+        menuBtnImg.setAttribute("src", "assets/shared/mobile/icon-hamburger.svg");
+        document.querySelector(".navigation").style.top = "-1000%";
+    }
+})
