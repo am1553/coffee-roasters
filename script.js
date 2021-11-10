160c186d445da628d528bd6bd84763cddb3d1045
am1553 <aryanmehta2016@gmail.com>
2021-11-10 16:44:27 +0000

    added text input in order summary

--- script.js
+++ script.js
@@ -33,18 +33,82 @@ for(let i = 0; i < accordionButton.length; i++) {
 }
 
 // SELECTING OPTIONS
-
 for(let i = 0; i < accordionButton.length; i++) {
     const accordionWrapper = accordionButton[i].nextElementSibling.children;
 
-    for(let z = 0; z < accordionWrapper.length; z++) {
-            accordionWrapper[z].addEventListener("click", ()=> {
+    for(let j = 0; j < accordionWrapper.length; j++) {
+        accordionWrapper[j].addEventListener("click", ()=> {
+            
+            for(let k = 0; k < accordionWrapper.length; k++) {
+                accordionWrapper[k].classList.remove("selected");
+            }
+            accordionWrapper[j].classList.add("selected");
+// -- -----------------------------------------------------adding text to inputText
+            const preference = document.querySelectorAll(".preference-button");
+            const bean = document.querySelectorAll(".bean-button");
+            const quantity = document.querySelectorAll(".quantity-button");
+            const grind = document.querySelectorAll(".grind-button");
+            const delivery = document.querySelectorAll(".delivery-button");
+
+            for(let i = 0; i < preference.length; i++) {
                 
-                for(let j = 0; j < accordionWrapper.length; j++) {
-                    accordionWrapper[j].classList.remove("selected");
+                if(preference[i].classList.contains("selected") === true) {
+                    const preferenceName = preference[i].children[0].innerHTML;
+                    preferenceInputText.innerText = preferenceName;
+                    preferenceInputTextId.innerText = preferenceName;
                 }
-                accordionWrapper[z].classList.add("selected");
+            }
+            for(let i = 0; i < bean.length; i++) {
                 
-            })
+                if(bean[i].classList.contains("selected") === true) {
+                    const beanName = bean[i].children[0].innerHTML;
+                    typeInputText.innerText = beanName;
+                    typeInputTextId.innerText = beanName;
+                }
+            }
+            for(let i = 0; i < quantity.length; i++) {
+                
+                if(quantity[i].classList.contains("selected") === true) {
+                    const quantityName = quantity[i].children[0].innerHTML;
+                    quantityInputText.innerText = quantityName;
+                    quantityInputTextId.innerText = quantityName;
+                }
+            }
+            for(let i = 0; i < grind.length; i++) {
+                
+                if(grind[i].classList.contains("selected") === true) {
+                    const grindName = grind[i].children[0].innerHTML;
+                    grindInputText.innerText = grindName;
+                    grindInputTextId.innerText = grindName;
+                }
+            }
+            for(let i = 0; i < delivery.length; i++) {
+                
+                if(delivery[i].classList.contains("selected") === true) {
+                    const deliveryName = delivery[i].children[0].innerHTML;
+                    deliveryInputText.innerText = deliveryName;
+                    deliveryInputTextId.innerText = deliveryName;
+                }
+            }
+            
+        })
     }
 }
+
+// OPTION INPUT IN SUMMARY
+// -- text-selectors-inputs
+const preferenceInputText = document.querySelector(".preference-input");
+const typeInputText = document.querySelector(".type-input");
+const quantityInputText = document.querySelector(".quantity-input");
+const grindInputText = document.querySelector(".grind-input");
+const deliveryInputText = document.querySelector(".delivery-input");
+
+const preferenceInputTextId = document.getElementById("preference-input");
+const typeInputTextId = document.getElementById("type-input");
+const quantityInputTextId = document.getElementById("quantity-input");
+const grindInputTextId = document.getElementById("grind-input");
+const deliveryInputTextId = document.getElementById("delivery-input");
+
+console.log(preferenceInputText);
+
+
